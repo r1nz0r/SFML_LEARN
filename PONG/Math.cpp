537d6b8c4c50efa39e3fe8844f6e124fafe31653
Alexey Boitsov <nicecontra1@gmail.com>
2023-12-04 21:07:06 +0300

    Deleted not in use functions from math.cpp

--- PONG/Math.cpp
+++ PONG/Math.cpp
@@ -1,18 +1,5 @@
 ﻿#include "Math.h"
 
-auto SetSpriteSize(sf::Sprite& sprite, const float desiredWidth, float desiredHeight) -> void
-{
-	const sf::FloatRect spriteRect = sprite.getLocalBounds();
-	const sf::Vector2f scale = { desiredWidth / spriteRect.width, desiredHeight / spriteRect.height };
-	sprite.setScale(scale);
-}
-
-void SetSpriteRelativeOrigin(sf::Sprite& sprite, const float originX, const float originY)
-{
-	const sf::FloatRect spriteRect = sprite.getLocalBounds();
-	sprite.setOrigin(originX * spriteRect.width, originY * spriteRect.height);
-}
-
 Vector2D GetNormalizedVector(const Vector2D& baseVector)
 {
 	const float length = sqrt(baseVector.x * baseVector.x + baseVector.y * baseVector.y);
--- PONG/Math.h
+++ PONG/Math.h
@@ -7,8 +7,6 @@ constexpr float STRAIGHT_ANGLE = 180.f;
 
 typedef sf::Vector2f Vector2D;
 
-void SetSpriteSize(sf::Sprite& sprite, float desiredWidth, float desiredHeight);
-void SetSpriteRelativeOrigin(sf::Sprite& sprite, float originX, float originY);
 bool IsNearlyEqual(const float numOne, const float numTwo);
 Vector2D GetNormalizedVector(const Vector2D& baseVector);
 float GetRadiansFromDegrees(const float degrees);
